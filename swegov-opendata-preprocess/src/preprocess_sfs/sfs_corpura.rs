25848d791b2fb897da062f16cfb49dbb5a937069
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-11-28 10:45:25 +0100

    feat: generate sparv-config for sfs

--- swegov-opendata-preprocess/src/preprocess_sfs/sfs_corpura.rs
+++ swegov-opendata-preprocess/src/preprocess_sfs/sfs_corpura.rs
@@ -1,6 +1,7 @@
 use std::{fs, path::Path};
 
 use error_stack::ResultExt;
+use sparv_extension::make_corpus_config;
 
 use crate::preprocess_sfs;
 use crate::PreprocessError;
@@ -12,17 +13,24 @@ pub struct PreprocessSfsCorpuraOptions<'a> {
 }
 
 pub fn preprocess_sfs_corpus(
-    options: PreprocessSfsCorpuraOptions,
+    PreprocessSfsCorpuraOptions { input, output }: PreprocessSfsCorpuraOptions<'_>,
 ) -> error_stack::Result<(), PreprocessError> {
-    tracing::info!("preprocess SFS corpus from {}", options.input.display());
-    for year in fs::read_dir(options.input).change_context(PreprocessError)? {
+    tracing::info!("preprocess SFS corpus from {}", input.display());
+    let corpus_id = "sfs";
+    make_corpus_config(
+        corpus_id,
+        "Svensk Författningssamling",
+        "",
+        &output.join(corpus_id),
+    )
+    .change_context(PreprocessError)?;
+    for year in fs::read_dir(input).change_context(PreprocessError)? {
         let year = year.change_context(PreprocessError)?.path();
         tracing::debug!("found path: {}", year.display());
         preprocess_sfs::build_sparv_source(
             year.as_path(),
-            &options
-                .output
-                .join("sfs")
+            &output
+                .join(corpus_id)
                 .join("source")
                 .join(year.file_stem().unwrap()),
         )?;
