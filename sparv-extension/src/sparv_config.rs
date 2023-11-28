57079d5a5d59f47529ff87f0bd3adc97134b2ce6
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-11-28 10:44:22 +0100

    fix: make sparv-config generate correct yaml

--- sparv-extension/src/sparv_config.rs
+++ sparv-extension/src/sparv_config.rs
@@ -1,20 +1,56 @@
 use std::io::Write;
-use std::path::PathBuf;
+use std::path::Path;
 use std::{fs, io};
 
 use error_stack::ResultExt;
 
 use crate::SparvConfigError;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SparvConfig {
+    parent: Option<String>,
+    metadata: SparvMetadata,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SparvConfigRef<'a> {
+    parent: Option<&'a str>,
+    metadata: &'a SparvMetadataRef<'a>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SparvMetadata {
+    id: String,
+    name: Vec<LangAndValue>,
+    description: Vec<LangAndValue>,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct SparvMetadataRef<'a> {
+    id: &'a str,
+    name: &'a [LangAndValueRef<'a>],
+    description: &'a [LangAndValueRef<'a>],
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct LangAndValue {
+    lang: String,
+    value: String,
+}
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct LangAndValueRef<'a> {
+    lang: &'a str,
+    value: &'a str,
+}
 /// Write Sparv corpus config file for sub corpus.
 pub fn make_corpus_config(
     corpus_id: &str,
     name: &str,
     descr: &str,
-    mut path: PathBuf,
+    path: &Path,
 ) -> error_stack::Result<(), SparvConfigError> {
     fs::create_dir_all(&path).change_context(SparvConfigError)?;
-    path.set_file_name("config.yaml");
+    let path = path.join("config.yaml");
     //     if config_file.is_file():
     //         return
     //     path.mkdir(parents=True, exist_ok=True)
@@ -38,14 +74,16 @@ pub fn make_corpus_config(
         .write("  name:\n".as_bytes())
         .change_context(SparvConfigError)?;
     writer
-        .write(format!("    swe: Riksdagens öppna data: {}\n", name).as_bytes())
+        .write(format!("    swe: 'Riksdagens öppna data: {}'\n", name).as_bytes())
         .change_context(SparvConfigError)?;
-    writer
-        .write("  description:\n".as_bytes())
-        .change_context(SparvConfigError)?;
-    writer
-        .write(format!("    swe: {}\n", descr).as_bytes())
-        .change_context(SparvConfigError)?;
+    if !descr.is_empty() {
+        writer
+            .write("  description:\n".as_bytes())
+            .change_context(SparvConfigError)?;
+        writer
+            .write(format!("    swe: '{}'\n", descr).as_bytes())
+            .change_context(SparvConfigError)?;
+    }
     //     with open(config_file, "w") as f:
     //         f.write(config_content)
     eprintln!("  Config {} written", path.display());
